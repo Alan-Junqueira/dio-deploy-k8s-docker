a0cdb307f129fb4ddde69ab27f222ca0029a59ee
alanjunqueira <contato.alanjunqueira@gmail.com>
2022-12-09 10:37:17 -0300

    first commit

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Criando as imagens..."
+
+docker build -t alanjunqueira/projeto-backend:1.0 backend/.
+docker build -t alanjunqueira/projeto-database:1.0 database/.
+
+echo "Realizando push das imagens..."
+
+docker push alanjunqueira/projeto-backend:1.0
+docker push alanjunqueira/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
+
